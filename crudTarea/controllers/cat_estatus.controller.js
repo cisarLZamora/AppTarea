aa4f044dabefaab4daf265e1da075129e75c7b43
cisarLZamora <cesarlealzamora@gmail.com>
2022-09-18 22:21:31 -0500

    modificacion controladores de los modelos crear/editar/eliminar/obtener
    Signed-off-by: cisarLZamora <cesarlealzamora@gmail.com>

--- crudTarea/controllers/cat_estatus.controller.js
+++ crudTarea/controllers/cat_estatus.controller.js
@@ -1,8 +1,91 @@
-const createCatTask = (req, res) => {
-    console.log(req.body)
-    res.send('creating catTask')
+const {cat_status} = require('../models/cat_estatus')
+
+const createCatTask = async(req, res) => {
+    const {nombre} = req.body;
+
+    try {
+        const newCat_task = await cat_status.create({
+            nombre : nombre
+        })
+        console.log(newCat_task)
+        res.json(newCat_task)
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: error.message
+        })
+    }
+}
+
+const getCatsTaskEstatus = async(req, res) => {
+    //array de tareas
+    try {
+        const categories = await cat_status.findAll()
+        console.log(categories);
+        res.json(categories)
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: error.message
+        })
+    }
+}
+
+const getCatTask = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const cat_task = await cat_status.findOne({
+            where: {
+                id
+            }
+        })
+        if (!cat_task) { 
+            return res.status(404).json({
+                mensaje: 'categoría no encontrada'
+            }) ;
+        }
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: error.message
+        }) 
+    }
+}
+
+const updateCatTaskEstatus = async(req, res) => {
+    try {
+        const {id} = req.params;
+        const {nombre} = req.body;
+        const cat = await cat_status.findByPk(id);
+
+        cat.nombre = nombre
+        await cat.save();
+        res.json(cat);
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: error.message
+        })
+    }
+}
+
+const deleteCatTaskEstatus = async(req, res) => {
+    const {id} = req.params;
+    try {
+        await cat_status.destroy({
+            where: {
+                id: id
+            }
+        })
+        res.sendStatus(204);
+        res.send('delete cat_estatus')
+    } catch (error) {
+        return res.status(500).json({
+            mensaje: error.message
+        })
+    }
 }
 
 module.exports={
-    createCatTask
+    createCatTask,
+    getCatsTaskEstatus,
+    updateCatTaskEstatus,
+    deleteCatTaskEstatus,
+    getCatTask
 }
--- crudTarea/routes/cat_estatus.routes.js
+++ crudTarea/routes/cat_estatus.routes.js
@@ -1,8 +1,13 @@
 const {Router} = require('express');
-const {createCatTask} = require('../controllers/cat_estatus.controller');
+const {createCatTask, deleteCatTaskEstatus, getCatsTaskEstatus
+, updateCatTaskEstatus, getCatTask} = require('../controllers/cat_estatus.controller');
 const catTaskRouter = new Router();
 
-catTaskRouter.get('/createCat', createCatTask)
+catTaskRouter.post('/createCat', createCatTask);
+catTaskRouter.get('/getCats', getCatsTaskEstatus);
+catTaskRouter.get('/getCat/:id', createCatTask);
+catTaskRouter.put('/updateCat/:id', updateCatTaskEstatus);
+
 
 module.exports = {
     catTaskRouter
